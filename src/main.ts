64a433a13cd674f376bbf28d8e10452b48dc7a30
eclero <eclero357@gmail.com>
2018-10-13 15:51:48 +0900

    firebaseと連携した

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import firebase from 'firebase';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+const config = {
+  apiKey: 'AIzaSyCIdk8lf09fbKcgGuMXa-kwOo9HCyFR9Zw',
+  authDomain: 'fire-chat-2aa0b.firebaseapp.com',
+  databaseURL: 'https://fire-chat-2aa0b.firebaseio.com',
+  projectId: 'fire-chat-2aa0b',
+  storageBucket: 'fire-chat-2aa0b.appspot.com',
+  messagingSenderId: '729648464323'
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
