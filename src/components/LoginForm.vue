d3e71e5e6953c4469b6aa507cb99be8cfe7a9824
eclero <eclero357@gmail.com>
2018-10-13 14:13:31 +0900

    下準備をした

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="login-form">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class LoginForm extends Vue {}
+</script>
