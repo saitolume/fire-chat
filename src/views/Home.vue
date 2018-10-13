aec9e9943cf532fd1a2c3bd8be2d604a2ca453a6
eclero <eclero357@gmail.com>
2018-10-13 14:13:31 +0900

    下準備をした

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1 class="msg" >Fire Chat</h1>
+    <img class="vue-logo" alt="Vue logo" src="../assets/logo.png">
+    <LoginForm />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import LoginForm from '@/components/LoginForm.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    LoginForm,
   },
 })
 export default class Home extends Vue {}
 </script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+
+.msg {
+  margin-top: 70px;
+}
+
+.vue-logo {
+  margin-top: 0px;
+}
+</style>
+
