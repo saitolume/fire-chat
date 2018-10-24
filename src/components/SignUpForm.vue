d7b60dc4bea7472ab1a1a6989a516502ba88d0ab
eclero <eclero357@gmail.com>
2018-10-24 19:43:26 +0900

    storeにuserを追加した

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="sign-up-form">
     <v-flex xs12 sm6 md3>
-      <!-- <v-text-field v-model="name" placeholder="ユーザー名"></v-text-field> -->
       <v-text-field v-model="email" placeholder="メールアドレス"></v-text-field>
       <v-text-field v-model="password" type="password" placeholder="パスワード"></v-text-field>
     </v-flex>
@@ -15,9 +14,8 @@ import firebase from 'firebase/app';
 
 @Component({})
 export default class SignUpForm extends Vue {
-  // @Provide() private name:     string = '';
-  @Provide() private email:    string = '';
-  @Provide() private password: string = '';
+  private email:    string = '';
+  private password: string = '';
 
   private signUp(): void {
     firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
