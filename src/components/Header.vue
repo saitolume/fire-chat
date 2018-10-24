f356877fc6251cac442d952ebc8d6d6eba595e26
eclero <eclero357@gmail.com>
2018-10-24 23:00:56 +0900

    ナビゲーション周りの改修

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,34 +1,39 @@
 <template>
   <div class="header">
     <v-toolbar class="white" app>
-      <v-btn @click.stop="drawer = !drawer" v-if="loginState()" icon large flat>
+      <v-btn @click.stop="drawer = !drawer" v-if="loginState" icon large flat>
         <v-icon>account_circle</v-icon>
       </v-btn>
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-btn to="/signin" v-if="!loginState()" flat>Sign in</v-btn>
+        <v-btn to="/signin" v-if="!loginState" flat>Sign in</v-btn>
       </v-toolbar-items>
     </v-toolbar>
 
     <v-navigation-drawer style="width:200px" v-model="drawer" absolute temporary>
-      <v-list>
+      <v-list class="pa-0">
         <v-list-tile avatar>
           <v-list-tile-avatar>
             <v-icon>account_circle</v-icon>
           </v-list-tile-avatar>
           <v-list-tile-content>
-            <v-list-tile-title>{{ name() }}</v-list-tile-title>
+            <v-list-tile-title>{{ name }}</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
-      <v-list>
-        <v-btn disabled large flat>Profiel</v-btn>
+
+      <v-divider></v-divider>
+
+      <v-list class="pa-1">
+        <v-btn disabled flat>Profiel</v-btn>
       </v-list>
-      <v-list>
-        <v-btn to="/setting" large flat>Setting</v-btn>
+
+      <v-list class="pa-1">
+        <v-btn to="/setting" flat>Setting</v-btn>
       </v-list>
-      <v-list>
-        <v-btn @click="signOut" large flat>Sign out</v-btn>
+
+      <v-list class="pa-1">
+        <v-btn @click="signOut" flat>Sign out</v-btn>
       </v-list>
     </v-navigation-drawer>
   </div>
@@ -42,11 +47,11 @@ import firebase from 'firebase/app';
 export default class Header extends Vue {
   private drawer: boolean = false;
 
-  private loginState(): boolean {
+  private get loginState(): boolean {
     return this.$store.getters.loginState;
   }
 
-  private name(): string {
+  private get name(): string {
     return this.$store.getters.name;
   }
 
