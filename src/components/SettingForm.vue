94338eb4b5085d62cf5e938a22ab5f3209ec4a3f
eclero <eclero357@gmail.com>
2018-10-27 17:07:06 +0900

    空かどうか確認するバリデーションを追加した

--- src/components/SettingForm.vue
+++ src/components/SettingForm.vue
@@ -2,7 +2,11 @@
   <div class="setting-form">
     <v-form>
       <v-flex xs12 sm6>
-        <v-text-field label="ユーザー名" v-model="name"></v-text-field>
+        <v-text-field
+          label="ユーザー名"
+          v-model="name"
+          :rules="[required]"
+        ></v-text-field>
       </v-flex>
       <v-btn @click="saveProfile" color="#42b983" outline>保存</v-btn>
     </v-form>
@@ -10,12 +14,16 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Provide } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 import firebase from 'firebase/app';
 
 @Component({})
 export default class SettingForm extends Vue {
   private name: string = this.$store.getters.name;
+
+  public required(value: string): string | boolean {
+    return value ? true : '入力してください';
+  }
 
   private saveProfile(): void {
     const user = firebase.auth().currentUser;
