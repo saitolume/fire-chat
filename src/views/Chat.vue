63cec242e30e69f40ac90c20fe25db8459e2bd30
eclero <eclero357@gmail.com>
2018-11-04 21:14:07 +0900

    メッセージを送信できるようにした

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,19 +1,78 @@
 <template>
   <div class="chat">
-    <h1>Chat</h1>
-    <p>開発中</p>
+    <div class="message-form">
+      <v-text-field
+        v-model="messageText"
+        placeholder="メッセージを入力"
+        solo
+      ></v-text-field>
+    </div>
+      <v-btn @click="sendMessage" color="success" small>送信</v-btn>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import firebase from 'firebase';
 
 @Component({})
-export default class Chat extends Vue {}
+export default class Chat extends Vue {
+  private messages:    string[] = [];
+  private messageText: string   = '';
+
+  private get name(): string {
+    return this.$store.getters.name;
+  }
+
+  private get loginState(): string {
+    return this.$store.getters.loginState;
+  }
+
+  private get uid(): string | null{
+    const user = firebase.auth().currentUser;
+    return user ? user.uid : null;
+  }
+
+  private sendMessage() {
+    if (this.loginState && this.messageText) {
+      firebase.firestore().collection('messages').add({
+        uid:  this.uid,
+        name: this.name,
+        text: this.messageText,
+        createdAt: Date.now(),
+      });
+    } else if (!this.loginState) {
+      alert('サインインしてください。');
+      this.$router.push('/');
+    } else if (!this.messageText) {
+      alert('文字を入力してください。');
+    } else {
+      alert('送信に失敗しました。')
+    }
+    this.messageText = '';
+  }
+}
 </script>
 
-<style scoped>
-h1 {
-  margin: 10px 0px 20px 0px;
+<style lang="scss" scoped>
+.message-form {
+  width: 77%;
+  margin-bottom: 28px;
+  position: absolute;
+  left: 0px;
+  bottom: 0px;
+  .v-input {
+    margin: 0px;
+    padding: 0px;
+  }
 }
+
+// button {
+//   margin: 0px;
+//   width: 23%;
+//   height: 49px;
+//   position: absolute;
+//   right: 0px;
+//   bottom: 56px;
+// }
 </style>
