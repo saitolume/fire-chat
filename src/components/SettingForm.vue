d3948aedb1dcdc616f579f5ef4b75125060c12be
eclero <eclero357@gmail.com>
2018-11-09 16:38:10 +0900

    実装できた（汚いコード）

--- src/components/SettingForm.vue
+++ src/components/SettingForm.vue
@@ -4,11 +4,27 @@
       <v-flex xs12 sm6>
         <v-text-field
           label="ユーザー名"
+          v-if="!loading && !saved"
           v-model="name"
           :rules="[required]"
         ></v-text-field>
+
+        <v-text-field
+          label="ユーザー名"
+          v-else-if="loading"
+          v-model="name"
+          loading
+        ></v-text-field>
+
+        <v-text-field
+          label="ユーザー名"
+          v-else-if="!loading && saved"
+          v-model="name"
+          @focus="saved = false"
+          success-messages="保存しました"
+        ></v-text-field>
       </v-flex>
-      <v-btn @click="saveProfile" color="#42b983" outline>保存</v-btn>
+      <v-btn @click="saveProfile" color="#42b983" :disabled="!name" outline>保存</v-btn>
     </v-form>
   </div>
 </template>
@@ -20,25 +36,25 @@ import firebase from 'firebase/app';
 @Component({})
 export default class SettingForm extends Vue {
   private name: string = this.$store.getters.name;
+  private loading: boolean = false;
+  private saved: boolean = false;
 
   private required(value: string): string | boolean {
     return value ? true : '入力してください';
   }
 
   private saveProfile(): void {
+    this.loading = true;
     const user = firebase.auth().currentUser;
-    if (user && this.name) {
+    if (user) {
       user.updateProfile({
         displayName: this.name,
         photoURL:    null,
       }).then(() => {
         this.$store.dispatch('updateName', this.name);
-        alert('保存しました。');
-      }).catch((error) => {
-        alert(error.message);
+        this.loading = false;
+        this.saved = true;
       });
-    } else if (!this.name) {
-      alert('ユーザー名を入力してください');
     }
   }
 }
