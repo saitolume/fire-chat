558a4f2a05fe8b17ae587fd7573eac68f7403179
eclero <eclero357@gmail.com>
2018-11-16 17:20:18 +0900

    ルーティングのダイナミックインポートをやめた

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import About from './views/About.vue';
+import Chat from './views/Chat.vue';
 import Home from './views/Home.vue';
+import Setting from './views/Setting.vue';
+import SignIn from './views/SignIn.vue';
+import SignUp from './views/SignUp.vue';
 
 Vue.use(Router);
 
@@ -8,35 +13,11 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/signin',
-      name: 'signin',
-      component: () => import('./views/SignIn.vue'),
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: () => import('./views/SignUp.vue'),
-    },
-    {
-      path: '/chat',
-      name: 'chat',
-      component: () => import('./views/Chat.vue'),
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('./views/About.vue'),
-    },
-    {
-      path: '/setting',
-      name: 'setting',
-      component: () => import('./views/Setting.vue'),
-    },
+    { path: '/',        name: 'home',    component: Home },
+    { path: '/signin',  name: 'signin',  component: SignIn },
+    { path: '/signup',  name: 'signup',  component: SignUp },
+    { path: '/chat',    name: 'chat',    component: Chat },
+    { path: '/about',   name: 'about',   component: About },
+    { path: '/setting', name: 'setting', component: Setting },
   ],
 });
